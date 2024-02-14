03eb49c52e81a282793e98b402037e485880ea9f
Andriamisa Nayann <99319983+nayannandriamisa@users.noreply.github.com>
2024-02-14 09:59:01 +0100

    Add files via upload

---
+++ src/main/java/fr/devavance/stationvelibservice/StationvelibServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.devavance.stationvelibservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationvelibServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationvelibServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/devavance/stationvelibservice/StationvelibServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.devavance.stationvelibservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationvelibServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
